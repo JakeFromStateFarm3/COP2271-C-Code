2c0f341b31650c6478a5240a49ce3896dd600cca
JakeFromStateFarm3 <wcastengera2007@gmail.com>
2026-02-23 04:25:13 +0000

    fbdfgfdgfd

---
+++ temprorary_file.c
@@ -0,0 +1,31 @@
+// part 1 number 1 {
+#include <stdio.h>
+
+int main(){
+    
+}
+// }
+//part 1 number 2 {
+//comment
+/*
+multi line comment
+hooray
+*/
+// }
+// part 1 number 3 {
+#include <math.h>
+//}
+// part 1 number 4 {
+/*
+so you take what number you have and break it down into a sum of powers
+of 2. 00000 is equal to zero. each place corresponds to a power
+of two. 00001 corresponds to one. 00010 corresponds to 2. 00011 corresponds
+to three. 00100 corresponds to 4 and so on.
+*/
+// }
+// part 1 number 5 {
+/*
+you take the binary number and find the power of 2 assigned to each place with a one in it.
+then simply add up the found powers of two
+*/
+// part 2 number 1 {
