cd3c25935ade7cdf0ffc2ba032bd8fe336afda35
JakeFromStateFarm3 <wcastengera2007@gmail.com>
2026-02-23 04:58:00 +0000

    fggfd

--- temprorary_file.c
+++ temprorary_file.c
@@ -3,7 +3,7 @@
 
 int main(){
     
-}
+
 // }
 //part 1 number 2 {
 //comment
@@ -14,6 +14,7 @@ hooray
 // }
 // part 1 number 3 {
 #include <math.h>
+#include <stdbool.h>
 //}
 // part 1 number 4 {
 /*
@@ -29,3 +30,100 @@ you take the binary number and find the power of 2 assigned to each place with a
 then simply add up the found powers of two
 */
 // part 2 number 1 {
+    int num;
+// }
+// part 2 number 2 {
+    int num2 = 3;
+// }
+// part 2 number 3 {
+int num3;
+float num4;
+double num5;
+char num6;
+bool num7;
+// }
+// part 2 number 4 {
+#define PI = 3.1415826535
+const float e = 2.718281828;
+// }
+// part 2 number 5 {
+num6 = 'g';
+num3 = (int) num6; //num6 was a char and this makes it an int
+// }
+// part 3 number 1 {
+printf("%d", num3);
+// }
+// part 3 number 2 {
+scanf("%f", &num4);
+//}
+// part 3 number 3 {
+printf("%d", num);
+printf("%f", num4);
+printf("%lf", num5);
+printf("%c", num6);
+// }
+// part 4 number 1 {
+// ==, >=, <=, >, <, !=
+// }
+// part 4 number 2 {
+// &&, ||, !
+// }
+// part 4 number 3 {
+// +, -, *, /, %
+// }
+// part 5 number 1 {
+if(1){
+
+}
+// }
+// part 5 number 2 {
+if(1){
+
+}
+else{
+
+}
+// }
+// part 5 number 3 {
+if(1){
+
+}
+else if(0){
+
+}
+else{
+
+}
+// }
+// part 5 number 4 {
+switch(num){
+    case 1:
+        break;
+    
+    case 2:
+        break;
+    
+}
+// }
+// part 6 number 1 {
+while(1){
+
+}
+// }
+// part 6 number 2 {
+do{
+
+}while(1);
+// }
+// part 6 number 3 {
+for(int i; 1; i++){
+
+}
+// }
+// part 6 number 4 {
+// break is to exit the innermost loop or to terminate a switch statement
+// }
+// part 6 number 5 {
+// to skip the remaining code in the loop and jump to the next iteration
+// }
+}
